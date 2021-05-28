8c51a950e6f7bc97325dffca17dd353839e398ef
Colin Sutter <Colin.Sutter@oregonsd.net>
2021-05-28 12:07:19 -0500

    Added Other Decompression Type

--- Get Data.py
+++ Get Data.py
@@ -3,22 +3,87 @@ from datetime import date
 from datetime import datetime
 from tqdm import tqdm
 import traceback
+import zstandard
 import requests
 import sqlite3
 import json
+import lzma
 import bz2
 import os
 
 PROGRESSBAR = False
 
 # Time Frame You Want To Download
-startDate = date(2010, 1, 1) # 2005 12 is the first available data
+startDate = date(2005, 12, 1) # 2005 12 is the first available data
 currentDate = startDate # Set The Date the is currently being downloaded to be the first date
-endDate = date(2017, 11, 1) # End of the time frame you want to download from
+endDate = date(2006, 1, 1) # End of the time frame you want to download from
 
 sqlConnection = sqlite3.connect("data.db")
 cursor = sqlConnection.cursor()
 sqlRequests = []
+
+def downloadData(timeframe):
+    try:
+        compressedData = b"" # Final Download Data, Compressed as BZ2, XZ, ZST
+        decompressedData = ""
+        fileType = ""
+
+        if timeframe < date(2017, 11, 2):
+            fileType = "bz2"
+
+        elif date(2017, 12, 1) <= timeframe <= date(2018, 10, 2):
+            fileType = "xz"
+
+        elif timeframe > date(2018, 11, 1):
+            fileType = "zst"
+
+        if PROGRESSBAR:
+            dataRequest = requests.get(f"https://files.pushshift.io/reddit/comments/RC_{timeframe.year}-{timeframe.month:02}.{fileType}", stream=True)
+            # Download Data Stream 
+            # Only good till 2017/11
+            totalDownloadSize = int(dataRequest.headers.get("Content-Length", 0))
+            # Total Download Data Size
+            
+            progressBar = tqdm(total=totalDownloadSize, unit="B", unit_scale=True, desc=f"File Download: {timeframe.year}-{timeframe.month:02}.{fileType}")
+            dataBlockSize = 1024 # How often the progress bar updates
+
+            for data in dataRequest.iter_content(dataBlockSize):
+                progressBar.update(len(data)) # Update Progress Bar by size of data
+                compressedData += data # Add Current Data to the currentData var
+            progressBar.close() # Stop Progress Bar
+
+        else:
+            print(f"Downloading: {timeframe.year}-{timeframe.month:02}")
+            dataRequest = requests.get(f"https://files.pushshift.io/reddit/comments/RC_{timeframe.year}-{timeframe.month:02}.{fileType}")
+            compressedData = dataRequest.content
+            # Download Data
+
+        if fileType == "bz2":
+            return bz2.decompress(compressedData)
+
+        elif fileType == "xz":
+            return lzma.decompress(compressedData)
+
+        elif fileType == "zst":
+            return zstandard.decompress(compressedData)
+
+    except MemoryError: # If you run out of memeory/ram
+        del decompressedData # Delete the current Decompressed Data
+        print("Out of Memory")
+        open(f"reddit/{currentDate.year}-{currentDate.month:02}.{fileType}", "wb").write(compressedData) # Open file your going to right in 
+
+        if fileType == "bz2":
+            file = bz2.open(f"reddit/{currentDate.year}-{currentDate.month:02}.{fileType}", "rb") # Reopen it as BZ2 object
+            return file.read()
+
+        elif fileType == "xz":        
+            file = lzma.open(f"reddit/{currentDate.year}-{currentDate.month:02}.{fileType}", "rb") # Reopen it as XZ object
+            return file.read()
+
+        elif fileType == "zst":
+            file = zstandard.open(f"reddit/{currentDate.year}-{currentDate.month:02}.{fileType}", "rb") # Reopen it as ZST object
+            return file.read()
+
 
 def SQLTransaction(request):
     global sqlRequests
@@ -137,7 +202,7 @@ def processData(line):
         # Check to see is parent is already replyed to and get reply score
         parentReplyScore = getParentReplyScore(parentID)
         if parentReplyScore: # If has reply
-            if score > parentReplyScore: # If Score is greater than existing parent reply
+            if score > int(parentReplyScore): # If Score is greater than existing parent reply
                 if acceptable(content): # Check to see if data is acceptable
                     replaceComment(commentID, parentID, parentContent, content, subreddit, createdUTC, score)
         else:
@@ -158,51 +223,18 @@ def processData(line):
             sqlConnection.commit()
 
 while currentDate != endDate:
-    compressedData = b"" # Final Download Data, Compressed as BZ2
-    decompressedData = ""
-
-    if PROGRESSBAR:
-        dataRequest = requests.get(f"https://files.pushshift.io/reddit/comments/RC_{currentDate.year}-{currentDate.month:02}.bz2", stream=True)
-        # Download Data Stream 
-        # Only good till 2017/11
-        totalDownloadSize = int(dataRequest.headers.get("Content-Length", 0))
-        # Total Download Data Size
-        
-        progressBar = tqdm(total=totalDownloadSize, unit="B", unit_scale=True, desc=f"File Download: {currentDate.year}-{currentDate.month:02}.bz2")
-        dataBlockSize = 1024 # How often the progress bar updates
-
-        for data in dataRequest.iter_content(dataBlockSize):
-            progressBar.update(len(data)) # Update Progress Bar by size of data
-            compressedData += data # Add Current Data to the currentData var
-        progressBar.close() # Stop Progress Bar
-
-    else:
-        print(f"Downloading: {currentDate.year}-{currentDate.month:02}")
-        dataRequest = requests.get(f"https://files.pushshift.io/reddit/comments/RC_{currentDate.year}-{currentDate.month:02}.bz2")
-        compressedData = dataRequest.content
-        # Download Data
-
     # Time to Process All of the Data
     cursor.execute("CREATE TABLE IF NOT EXISTS parentReply(parentID TEXT PRIMARY KEY, commentID TEXT UNIQUE, parent TEXT, comment TEXT, subreddit TEXT, unix INT, score INT)")
     # Create Table if it does not exist
     startRow = 0
 
-    try: # To catch when you run out of memery/ram
-        for line in tqdm(bz2.decompress(compressedData).splitlines(), desc=f"Lines in {currentDate.year}-{currentDate.month:02}"):
-            rowCounter += 1 # Increment Row Counter 
-            if rowCounter > startRow:
-                processData(line)
+    print("Done Downloading, Starting To Decompress")
+    decompressedData = downloadData(currentDate)
+    for line in tqdm(decompressedData.splitlines(), desc=f"Lines in {currentDate.year}-{currentDate.month:02}"):
+        rowCounter += 1 # Increment Row Counter 
+        if rowCounter > startRow:
+            processData(line)
 
-    except MemoryError: # If you run out of memeory/ram
-        del decompressedData # Delete the current Decompressed Data
-        open(f"reddit/{currentDate.year}-{currentDate.month:02}.bz2", "wb").write(compressedData) # Open file your going to right in 
-        file = bz2.open(f"reddit/{currentDate.year}-{currentDate.month:02}.bz2", "rb") # Reopen it as BZ2 object
-        for line in file: # For line in file
-            processData(line) # Process Line
-
-        print("Out of Memory")
-
-    del compressedData # Clean up memory
     del decompressedData
 
     currentDate += relativedelta(months=+1) # Add One Month To Current Date
