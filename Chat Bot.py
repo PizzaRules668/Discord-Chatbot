5405f4c3587dc97568d6d427d31660f3f6778b98
Colin Sutter <Colin.Sutter@oregonsd.net>
2021-06-07 10:59:39 -0500

    Added TTS and !join/!leave

--- Chat Bot.py
+++ Chat Bot.py
@@ -1,19 +1,17 @@
 from model.inference import inference # Used to get reply
 from discord.ext.commands import Bot # Discord Bot 
 from dotenv import load_dotenv # Load Secret Keys
+from gtts import gTTS # Used For TTS
 import discord # Base Discord
-import pyttsx3 # Used For TTX
 import json # Used for loading settings
 import os # Used to Get Secret Keys
 
 class ChatBot(Bot): # Make Bot
     def __init__(self, command_prefix, self_bot=False):
         Bot.__init__(self, command_prefix=command_prefix, self_bot=self_bot) # Init Bot 
-    
+
         self.registerEvents() # Register All Discord Events that we are going to use (on_message)
         self.registerCommands() # Register All Discord Commands that we are going to use (join)
-
-        self.engine = pyttsx3.init() # Init pyttsx3 Engine
 
         self.inVC = False
         self.vc = None
@@ -25,7 +23,7 @@ class ChatBot(Bot): # Make Bot
         @self.event
         async def on_message(message): # Register On Message Event
             await self.process_commands(message)
-            if message.author != self.user: # If message not from user
+            if message.author != self.user and not message.content.startswith("!"): # If message not from user
                 try:
                     answers = inference(message.content) # Inference on message content  
                     if not self.inVC:
@@ -36,12 +34,17 @@ class ChatBot(Bot): # Make Bot
 
                     elif self.inVC:
                         try:
-                            self.engine.save_to_file(answers["answers"][answers["best_index"]], "message.wav") # Try and get the best message
+                            tts = gTTS(text=answers["answers"][answers["best_index"]], lang="en")
+                            tts.save("message.mp3")
 
-                        except:
-                            self.engine.save_to_file(answers["answers"][0], "message.wav") # Else Get the first answer
+                        except Exception as e:
+                            print(e)
+                            tts = gTTS(text=answers["answers"][0], lang="en")
+                            tts.save("message.mp3")
+                            
                         
-                        self.vc.play(discord.FFmpegPCMAudio(source="message.wav"))
+                        self.vc.play(discord.FFmpegPCMAudio(source="message.mp3"))
+                        os.remove("message.mp3")
 
                 except Exception as e: # If error
                     await message.channel.send("**ERROR** Please Check Console")
@@ -59,8 +62,10 @@ class ChatBot(Bot): # Make Bot
 
         @self.command()
         async def leave(message):
-            if self.inVC:
-                pass
+            if not self.inVC:
+                return
+
+            await message.guild.voice_client.disconnect()
 
 load_dotenv()
 if __name__ == "__main__":
